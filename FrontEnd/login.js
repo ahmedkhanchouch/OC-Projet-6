29140d705218173018b0b3c8c1899319f7aef125
ahmed khanchouch <akhanchouch93@gmail.com>
2024-01-17 21:33:26 +0100

    travaill sur la modal

---
+++ FrontEnd/login.js
@@ -0,0 +1,27 @@
+const reponse = fetch("http://localhost:5678/api/users/POST/login");
+console.log(reponse)
+	
+//     const form = document.querySelector('#contact');
+//     form.addEventListener("submit", (event) => {
+//         // On empêche le comportement par défaut
+//         event.preventDefault();
+    
+//         // On récupère les deux champs et on affiche leur valeur
+//         const email = document.getElementById("email").value;
+//         const password = document.getElementById("password").value;
+// 		// const emailv = event.target.dataset.email;
+        
+// 		if(email === EmailSophie ){
+//         console.log(ok)
+//        } else{
+//         console.log("Une erreur est survenue : ")
+//        }
+
+       
+       
+       
+       
+       
+//         // console.log(password);
+//         // console.log(email);
+// });
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -72,3 +72,29 @@ function genererPieces(pieces){
 
 
         
+
+        // modal bloc 
+        let modal = null 
+
+        const openModal = function (e) {
+            e.preventDefault();
+            const target = document.querySelector (e.target.getAttribute('href'))
+            target.style.display = null 
+            target.removeAttribute('aria-hidden')
+            target.setAttribute('aria-modal','true')
+            modal = target
+            modal.addEventListener('click' , closeModal)
+        }
+        const closeModal = function(e){
+            if ( modal === null) return
+            e.preventDefault();
+            modal.style.display = "none" 
+            modal.setAttribute('aria-hidden' , 'true')
+            modal.removeAttribute('aria-modal','true')
+            modal.removeEventListener('click' , closeModal)
+            modal = null
+            
+        }
+        document.querySelectorAll('.js-modal').forEach(a => {
+            a.addEventListener('click' , openModal)
+        })
