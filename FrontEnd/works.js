6e6eb0acf97ccadd24c91f3256dc1612ff9abbfa
ahmed khanchouch <akhanchouch93@gmail.com>
2024-01-22 19:12:36 +0100

    creation de deuxieme modal

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -25,9 +25,33 @@ function genererPieces(pieces){
      }
      
     }
+function genererPiecesmodal(pieces){
+    for (let i = 0; i < pieces.length; i++) {
+
+        const figure = pieces[i];
+        // Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionFiches = document.querySelector(".gallery-modal");
+        // Création d’une balise dédiée à une figure d'images
+        const pieceElement = document.createElement("figure");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+        // creatio de buttopn supprimer 
+        const imagedelete =document.createElement("button");
+        imagedelete.setAttribute('class','js-modal-delete')
+        imagedelete.innerHTML = '<i class="fa-solid fa-trash-can"></i>' 
+        // On rattache la balise figure a la section Fiches
+        sectionFiches.appendChild(pieceElement);
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(imagedelete)
+    
+     }
+     
+    }
     
     
         genererPieces(pieces);
+        genererPiecesmodal(pieces);
 
 
         const boutonAll = document.querySelector(".btn-all");
@@ -84,17 +108,32 @@ function genererPieces(pieces){
             target.setAttribute('aria-modal','true')
             modal = target
             modal.addEventListener('click' , closeModal)
+            modal.querySelector('.js-modal-close').addEventListener('click' , closeModal)
+            modal.querySelector('.js-modal-stop').addEventListener('click' , stopPropagation)
         }
         const closeModal = function(e){
             if ( modal === null) return
             e.preventDefault();
             modal.style.display = "none" 
             modal.setAttribute('aria-hidden' , 'true')
-            modal.removeAttribute('aria-modal','true')
+            modal.removeAttribute('aria-modal')
             modal.removeEventListener('click' , closeModal)
+            modal.querySelector('.js-modal-close').removeEventListener('click' , closeModal)
+            modal.querySelector('.js-modal-stop').removeEventListener('click' , stopPropagation)
             modal = null
             
         }
+
+        const stopPropagation = function (e){
+            e.stopPropagation();
+        }
+        
         document.querySelectorAll('.js-modal').forEach(a => {
             a.addEventListener('click' , openModal)
+        })
+
+        window.addEventListener('keydown' , function (e) {
+            if (e.key === 'escape' || e.key === 'esc') {
+                closeModal(e)
+            }
         })
